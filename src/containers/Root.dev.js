eb8f42214d2f2f17c77bfa388d67f5b8bc0411c9
Mak <dtlvan@gmail.com>
2016-04-06 17:31:25 +0700

    add Root Container

---
+++ src/containers/Root.dev.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import DevTools from './DevTools';
+
+export default class Root extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <Provider store={store}>
+        <div>
+          Add Main Component here
+          <DevTools />
+        </div>
+      </Provider>
+    );
+  }
+}
